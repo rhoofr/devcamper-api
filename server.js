7dd51c7437ef25c3deabe2b7f8175f3f32b042ef
Richard Hoofring <rhoofr@yahoo.com>
2021-04-03 22:20:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`
+  );
+});
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.send('howdy');
+});
