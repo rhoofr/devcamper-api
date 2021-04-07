d5e51081670fe8880dc2ba5ed0558b3d44b396fe
Richard Hoofring <rhoofr@yahoo.com>
2021-04-06 21:33:41 -0400

    add prevent nosql injection, sanitize mongo data, xss protection, security headers, rate limiting, hpp and cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const fileupload = require('express-fileupload');
 const morgan = require('morgan');
 const moment = require('moment-timezone');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const { getCurrentDateTimeLocal } = require('./utils/datetime');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -46,6 +52,28 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 1000
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
