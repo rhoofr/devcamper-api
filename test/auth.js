5b92d291f3df53cf90d1d4c5d77656bf80de1844
Richard Hoofring <rhoofr@yahoo.com>
2021-04-21 20:12:44 -0400

    modify error middleware to log and error if development mode and modify auth get user test

--- test/auth.js
+++ test/auth.js
@@ -206,12 +206,12 @@ describe('-- Auth Testing --', () => {
           res.should.have.status(200);
           res.body.should.be.a('object');
           res.body.should.have.property('success').eql(true);
-          res.body.should.have.property('data');
-          res.body.data.should.be.a('object');
-          res.body.data.should.have.property('role');
-          res.body.data.should.have.property('_id');
-          res.body.data.should.have.property('name');
-          res.body.data.should.have.property('email');
+          res.body.should.have.property('user');
+          res.body.user.should.be.a('object');
+          res.body.user.should.have.property('role');
+          res.body.user.should.have.property('_id');
+          res.body.user.should.have.property('name');
+          res.body.user.should.have.property('email');
           done();
         });
     });
